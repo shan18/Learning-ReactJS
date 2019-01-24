ff72d026c3b6ea2adb8f05f12b6bc6ec5b58d8e6
Shantanu <thegeek.004@gmail.com>
2019-01-25 01:57:46 +0530

    add jsx code with styling and className

--- src/index.js
+++ src/index.js
@@ -1,11 +1,25 @@
 // Import the React and ReactDOM libraries
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function getButtonText() {
+  return 'Click on me!';
+}
 
 // Create a React Component
 const App = () => {
-  return <div>Hi There!</div>;
+  return (
+    <div>
+      <label className="label" for="name">
+        Enter Name:
+      </label>
+      <input id="name" type="text" />
+      <button style={{ backgroundColor: 'blue', color: 'white' }}>
+        {getButtonText()}
+      </button>
+    </div>
+  );
 };
 
 // Take the react component and show it on the screen
-ReactDOM.render(<App />, document.querySelector("#root"));
+ReactDOM.render(<App />, document.querySelector('#root'));
