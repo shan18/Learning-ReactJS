d98a9264e4f5469391cd3a562ce23b9d39f5777e
Shantanu <thegeek.004@gmail.com>
2019-01-25 20:00:45 +0530

    change for attribute to htmlFor

--- src/index.js
+++ src/index.js
@@ -2,20 +2,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-function getButtonText() {
-  return 'Click on me!';
-}
-
 // Create a React Component
 const App = () => {
+  const buttonText = 'Click me';
+
   return (
     <div>
-      <label className="label" for="name">
+      <label className="label" htmlFor="name">
         Enter Name:
       </label>
       <input id="name" type="text" />
       <button style={{ backgroundColor: 'blue', color: 'white' }}>
-        {getButtonText()}
+        {buttonText}
       </button>
     </div>
   );
