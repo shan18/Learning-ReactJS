34deaf50e8296a74393c8d684b369b0ea86c57ca
Shantanu <thegeek.004@gmail.com>
2019-01-27 02:11:44 +0530

    Update to class component

--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const App = () => {
-  // Get user location
-  window.navigator.geolocation.getCurrentPosition(
-    // This can return two function callbacks
-    position => console.log(position), // Success callback
-    err => console.log(err) // Faliure callback
-  );
+class App extends React.Component {
+  render() {
+    // Get user location
+    window.navigator.geolocation.getCurrentPosition(
+      // This can return two function callbacks
+      position => console.log(position), // Success callback
+      err => console.log(err) // Faliure callback
+    );
 
-  return <div>Hi there.</div>;
-};
+    return <div>Latitude: </div>;
+  }
+}
 
 ReactDOM.render(<App />, document.querySelector('#root'));
