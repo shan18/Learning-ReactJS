fe6f9945ae9b2ec60aae34d6d602ede92cd7ead1
Shantanu <thegeek.004@gmail.com>
2019-01-28 12:45:29 +0530

    display correct season with icon

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,7 +1,26 @@
 import React from 'react';
 
-const SeasonDisplay = () => {
-  return <div>Season Display.</div>;
+const getSeason = (lat, month) => {
+  // Date() returns month with 0-indexed values
+  if (month > 2 && month < 9) {
+    return lat > 0 ? 'summer' : 'winter'; // lat > 0 for northern hemisphere
+  } else {
+    return lat > 0 ? 'winter' : 'summer';
+  }
+};
+
+const SeasonDisplay = props => {
+  const season = getSeason(props.lat, new Date().getMonth());
+  const text =
+    season === 'winter' ? "Burr, it's chilly!" : "Let's hit the beach!";
+  const icon = season === 'winter' ? 'snowflake' : 'sun';
+  return (
+    <div>
+      <i className={`${icon} icon`} />
+      <h1>{text}</h1>
+      <i className={`${icon} icon`} />
+    </div>
+  );
 };
 
 export default SeasonDisplay;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import SeasonDisplay from './SeasonDisplay';
+
 class App extends React.Component {
   state = { lat: null, errorMessage: '' }; // Babel will do this initialization inside a constructor
 
@@ -33,7 +35,7 @@ class App extends React.Component {
       return <div>Error: {this.state.errorMessage}</div>;
     }
     if (!this.state.errorMessage && this.state.lat) {
-      return <div>Latitude: {this.state.lat}</div>;
+      return <SeasonDisplay lat={this.state.lat} />;
     }
     return <div>Loading!</div>;
   }
