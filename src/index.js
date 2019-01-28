a167f5d5a46f0a3da8e35c18b7b3264b01525504
Shantanu <thegeek.004@gmail.com>
2019-01-28 13:22:53 +0530

    add loading spinner

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import SeasonDisplay from './SeasonDisplay';
+import Spinner from './Spinner';
 
 class App extends React.Component {
   state = { lat: null, errorMessage: '' }; // Babel will do this initialization inside a constructor
@@ -37,7 +38,7 @@ class App extends React.Component {
     if (!this.state.errorMessage && this.state.lat) {
       return <SeasonDisplay lat={this.state.lat} />;
     }
-    return <div>Loading!</div>;
+    return <Spinner />;
   }
 }
 
