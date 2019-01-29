1ae109a23ce322e93adb60bd143696191942e7b8
Shantanu <thegeek.004@gmail.com>
2019-01-30 02:01:15 +0530

    v2 complete: display image in tiled grid format

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,13 +1,12 @@
 import './ImageList.css';
 import React from 'react';
+import ImageCard from './ImageCard';
 
 const ImageList = props => {
-  // images is a list of JS objects, in map if we name specific
-  // attributes of the object, it will load only those specific attribute values.
-  const images = props.images.map(({ description, id, urls }) => {
+  const images = props.images.map(image => {
     // we add the key property because this allows react to efficiently
     // compare items in the list and current DOM and re-render the page
-    return <img key={id} alt={description} src={urls.regular} />;
+    return <ImageCard key={image.id} image={image} />;
   });
   return <div className="image-list">{images}</div>;
 };
