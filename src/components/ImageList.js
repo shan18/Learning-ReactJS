5c60ee51233107ef734d9f3b77d6f2798667554e
Shantanu <thegeek.004@gmail.com>
2019-01-30 02:28:52 +0530

    Update README

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -4,8 +4,9 @@ import ImageCard from './ImageCard';
 
 const ImageList = props => {
   const images = props.images.map(image => {
-    // we add the key property because this allows react to efficiently
-    // compare items in the list and current DOM and re-render the page
+    // the key property allows react to efficiently compare items
+    // in the list and current DOM and re-render the page
+    // thus we always add the key property to the root element rendering the list
     return <ImageCard key={image.id} image={image} />;
   });
   return <div className="image-list">{images}</div>;
