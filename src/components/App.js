d20bbe3861d87f987f7fb667fbec898702df4f63
Shantanu <thegeek.004@gmail.com>
2019-01-29 20:32:25 +0530

    make requests to unsplash api with axios and promises

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import axios from 'axios';
 import SearchBar from './SearchBar';
 
 class App extends React.Component {
   onSearchSubmit(term) {
-    console.log(term);
+    const accessKey = process.env.REACT_APP_UNSPLASH_API_ACCESS_KEY;
+    axios
+      .get('https://api.unsplash.com/search/photos', {
+        params: { query: term },
+        headers: {
+          Authorization: `Client-ID ${accessKey}`
+        }
+      })
+      .then(response => {
+        console.log(response.data.results);
+      });
   }
 
   render() {
