5d7eaa8029a7d5f02e429b771abab6952bc15c5f
Shantanu <thegeek.004@gmail.com>
2019-01-30 16:51:04 +0530

    Add search bar functionality

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,17 @@
 import React from 'react';
+import SearchBar from './SearchBar';
 
 class App extends React.Component {
+  onSearchSubmit(term) {
+    console.log(term);
+  }
+
   render() {
-    return <div>Hello</div>;
+    return (
+      <div className="ui container" style={{ marginTop: '10px' }}>
+        <SearchBar onSubmit={this.onSearchSubmit} />
+      </div>
+    );
   }
 }
 
