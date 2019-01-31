6abd4d7fe0cd3fff82cf14b4051009928ee30f33
Shantanu <thegeek.004@gmail.com>
2019-01-31 16:52:05 +0530

    combine reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,3 +1,5 @@
+import { combineReducers } from 'redux';
+
 // Reducers
 
 const songsReducer = () => {
@@ -16,3 +18,8 @@ const selectedSongReducer = (selectedSong = null, action) => {
   }
   return selectedSong;
 };
+
+export default combineReducers({
+  songs: songsReducer,
+  selectedSong: selectedSongReducer
+});
