93ffe5731b4a3e9254eb1b7908dadbdd80cbfef6
Shantanu <thegeek.004@gmail.com>
2019-02-01 19:15:16 +0530

    integrate redux-thunk

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,13 @@
-export const fetchPosts = () => {
-  return {
-    type: 'FETCH_POSTS'
-  };
+import jsonPlaceholder from '../apis/jsonPlaceholder';
+
+// async await will just modify the return values of the inner function
+// but redux-thunk does not use the return values of that function
+// so it won't matter whether we return something from the inner function or not
+//
+// If we want to use the getState function, then we can use it by adding a parameter
+// to the inner function:
+// export const fetchPosts = () => async (dispatch, getState) => {
+export const fetchPosts = () => async dispatch => {
+  const response = await jsonPlaceholder.get('/posts');
+  dispatch({ type: 'FETCH_POSTS', payload: response });
 };
