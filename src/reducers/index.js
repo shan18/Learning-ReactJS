f2cdfff2632ab5dbac8f306dbf1691c9bf86d82f
Shantanu <thegeek.004@gmail.com>
2019-02-03 15:55:27 +0530

    add post reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,7 @@
 import { combineReducers } from 'redux';
 
+import postsReducer from './postsReducer';
+
 export default combineReducers({
-  dummyKey: () => 'just dummy for boilerplate code'
+  posts: postsReducer
 });
