dcb0297586785d82314298979b16e7ffb498dd13
Shantanu <thegeek.004@gmail.com>
2019-02-04 02:30:02 +0530

    update README

--- src/actions/index.js
+++ src/actions/index.js
@@ -30,6 +30,13 @@ export const fetchPosts = () => async dispatch => {
   dispatch({ type: 'FETCH_POSTS', payload: response.data });
 };
 
+// The above function is just a shortened version of:
+// fetchPosts = function() {
+//   return async function(dispatch) {
+//     ...
+//   }
+// }
+
 export const fetchUser = id => async dispatch => {
   const response = await jsonPlaceholder.get(`/users/${id}`);
   dispatch({ type: 'FETCH_USER', payload: response.data });
