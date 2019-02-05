5c0de19c2479bf9bdea00a84ab5fcdca9b7a84f6
Shantanu <thegeek.004@gmail.com>
2019-02-05 19:31:33 +0530

    add react router

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,7 +1,32 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+const PageOne = () => {
+  return (
+    <div>
+      <h1>Page One</h1>
+    </div>
+  );
+};
+
+const PageTwo = () => {
+  return (
+    <div>
+      <h1>Page Two</h1>
+      <button>Click Me!</button>
+    </div>
+  );
+};
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <BrowserRouter>
+      <div>
+        <Route path="/" exact component={PageOne} />
+        <Route path="/pagetwo" exact component={PageTwo} />
+      </div>
+    </BrowserRouter>
+  );
 };
 
 export default App;
