e3a5970c010f00eeebfba615aa6ee41484e381c0
Shantanu <thegeek.004@gmail.com>
2019-02-05 21:30:58 +0530

    navigation with <Link />

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
 
 const PageOne = () => {
   return (
     <div>
       <h1>Page One</h1>
+      <Link to="/pagetwo">Navigate to page two</Link>
     </div>
   );
 };
@@ -14,6 +15,7 @@ const PageTwo = () => {
     <div>
       <h1>Page Two</h1>
       <button>Click Me!</button>
+      <Link to="/">Navigate to page one</Link>
     </div>
   );
 };
