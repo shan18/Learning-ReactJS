21313e55bfbadaa753db584931c7201970415082
Shantanu <thegeek.004@gmail.com>
2019-03-08 16:24:19 +0530

    create context objext

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import LanguageContext from '../context/LanguageContext';
 
 class Button extends React.Component {
+  // The variable name should be contextType
+  static contextType = LanguageContext;
+
   render() {
+    console.log(this.context);
     return <button className="ui button primary">Submit</button>;
   }
 }
---
+++ src/context/LanguageContext.js
@@ -0,0 +1,4 @@
+import React from 'react';
+
+// Providing a default value of 'english'
+export default React.createContext('english');
