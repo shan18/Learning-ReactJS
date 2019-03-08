339201095518627a01483fb55af97bf566056ec1
Shantanu <thegeek.004@gmail.com>
2019-03-08 20:28:31 +0530

    add another context to change button color

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import UserCreate from './UserCreate';
 import LanguageContext from '../context/LanguageContext';
+import ColorContext from '../context/ColorContext';
 
 class App extends React.Component {
-  state = { language: 'english' };
+  state = { language: 'english', color: 'primary' };
 
   onLanguageChange = language => {
     this.setState({ language });
+  };
+
+  onColorChange = color => {
+    this.setState({ color });
   };
 
   render() {
@@ -23,9 +28,31 @@ class App extends React.Component {
             onClick={() => this.onLanguageChange('dutch')}
           />
         </div>
-        <LanguageContext.Provider value={this.state.language}>
-          <UserCreate />
-        </LanguageContext.Provider>
+
+        <div>
+          Select a button color:
+          <button
+            className="ui button primary"
+            onClick={() => this.onColorChange('primary')}
+          >
+            Blue
+          </button>
+          <button
+            className="ui button red"
+            onClick={() => this.onColorChange('red')}
+          >
+            Red
+          </button>
+        </div>
+
+        {/* It does not matter whether we wrap LanguageContext inside
+        ColorContext or the other way around. We just have to wrap
+        UserCreate inside both the Providers. */}
+        <ColorContext.Provider value={this.state.color}>
+          <LanguageContext.Provider value={this.state.language}>
+            <UserCreate />
+          </LanguageContext.Provider>
+        </ColorContext.Provider>
       </div>
     );
   }
---
+++ src/context/ColorContext.js
@@ -0,0 +1,3 @@
+import React from 'react';
+
+export default React.createContext(); // we don't provide a default value here
