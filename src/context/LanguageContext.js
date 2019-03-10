0d864dd0f7ec789982b9dde122c0d5ed726417b5
Shantanu <thegeek.004@gmail.com>
2019-03-10 10:39:02 +0530

    create a language store component

--- src/context/LanguageContext.js
+++ src/context/LanguageContext.js
@@ -1,4 +1,37 @@
 import React from 'react';
 
-// Providing a default value of 'english'
-export default React.createContext('english');
+// We always use a captial letter for naming components
+// because if we use all lower case letters and write some
+// JSX with that component then React will think of the component
+// tag as some vanilla HTML tag.
+const Context = React.createContext('english'); // Providing a default value of 'english'
+
+export class LanguageStore extends React.Component {
+  state = { language: 'english' };
+
+  onLanguageChange = language => {
+    this.setState({ language });
+  };
+
+  render() {
+    console.log(this.props.children);
+    return (
+      <Context.Provider
+        value={{ ...this.state, onLanguageChange: this.onLanguageChange }}
+      >
+        {/* We need to wrap all the components inside the Context.Provider so that they can
+        access the context data.
+        Context.Provider tag cannot be used in multiple places because each instance will
+        create a new pipeline. Thus, we need to wrap all the components inside Context.Provider
+        in a single place.
+        props.children will contain all the components that have been wrapped
+        around LanguageStore in other components.
+        Thus, by wrapping props.children inside Context.Provider, we can wrap all the
+        components in a single place. */}
+        {this.props.children}
+      </Context.Provider>
+    );
+  }
+}
+
+export default Context;
