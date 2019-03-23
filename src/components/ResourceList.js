286a14c33f690bee43635e440b76f954d9ec0088
Shantanu <thegeek.004@gmail.com>
2019-03-23 18:18:58 +0530

    put api request logic within the useEffect

--- src/components/ResourceList.js
+++ src/components/ResourceList.js
@@ -4,15 +4,13 @@ import axios from 'axios';
 const ResourceList = ({ resource }) => {
   const [resources, setResources] = useState([]);
 
-  const fetchResource = async resource => {
-    const response = await axios.get(
-      `http://jsonplaceholder.typicode.com/${resource}`
-    );
-    setResources(response.data);
-  };
-
   useEffect(() => {
-    fetchResource(resource);
+    (async resource => {
+      const response = await axios.get(
+        `http://jsonplaceholder.typicode.com/${resource}`
+      );
+      setResources(response.data);
+    })(resource);
   }, [resource]);
 
   return <div>{resources.length}</div>;
