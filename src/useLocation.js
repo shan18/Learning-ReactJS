e6bae4c54c180f971df52e067fda953e2e3643bf
Shantanu <thegeek.004@gmail.com>
2019-03-26 00:24:09 +0530

    extract hook logic to a separate file

---
+++ src/useLocation.js
@@ -0,0 +1,17 @@
+import { useState, useEffect } from 'react';
+
+export default () => {
+  const [lat, setLat] = useState(null);
+  const [errorMessage, setErrorMessage] = useState('');
+
+  useEffect(() => {
+    window.navigator.geolocation.getCurrentPosition(
+      position => setLat(position.coords.latitude),
+      err => setErrorMessage(err.message)
+    );
+  }, []);
+
+  // community convention is to return values
+  // as an array when using hooks
+  return [lat, errorMessage];
+};
